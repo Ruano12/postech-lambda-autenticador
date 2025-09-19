5dbd1ad2bee8f47c7b86b45233f8a7a06e004a5a
Marcelo Benjamim <mah.benjamim12@gmail.com>
2025-09-19 15:52:06 -0300

    primeiro commit, esteira e tf

--- infra/main.tf
+++ infra/main.tf
@@ -23,10 +23,6 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-provider "aws" {
-  region = var.aws_region
-}
-
 data "aws_vpc" "postech_vpc" {
   filter {
     name   = "tag:Name"
